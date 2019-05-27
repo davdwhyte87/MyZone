c4437e3209248b2b94221f05b27ce90abb9cadba
David whyte <kingstonwhyte87@gmail.com>
2019-05-27 10:05:23 +0100

    Add tests

---
+++ MyZoneTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using MyZone.Utils;
+using NUnit.Framework;
+using System;
+using MyZone;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Passwords passwords = new Passwords();
+            string RealPassword = "skdjkkfkf";
+            string HashedPassword = passwords.EncryptPassword(RealPassword);
+            Console.Write(HashedPassword);
+            Assert.IsNotEmpty(HashedPassword);
+        }
+    }
+}
